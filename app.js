50912d28d9d0c48879ee7e9ef8b3302fd06363c2
oripovnodirbek <nodirbekoripov1333@gmail.com>
2024-04-21 23:31:38 +0500

    js

---
+++ app.js
@@ -0,0 +1,116 @@
+// const num = +prompt("Son kiriting");
+// let result;
+// if (num > 0) {
+//   result = num + 1;
+//   alert(result);
+// } else {
+//   alert(num);
+// }
+
+
+// const num = +prompt("Son kiriting");
+// let result;
+// if (num > 0) {
+//   result = num + 1;
+//   alert(result);
+// } else {
+//     result = num - 2;
+//   alert(result);
+// }
+
+
+// const num = +prompt("Son kiriting");
+// let result;
+// if (num > 0) {
+//   result = num + 1;
+//   alert(result);
+// } if (num < 0) {
+//     result = num - 2
+//   alert(result);
+// } else {
+//     alert(10)
+// }
+
+
+// const num1 = +prompt("Birinchi sonni kiriting");
+// const num2 = +prompt("Ikkinchi sonni kiriting");
+// const num3 = +prompt("Uchinchi sonni kiriting");
+// let counter = 0;
+// let result;
+// if (num1 > 0) {
+//   counter++;
+// }
+// if (num2 > 0) {
+//   counter++;
+// }
+// if (num3 > 0) {
+//   counter++;
+// }
+// result = counter;
+// alert(result + ` ta musbat son bor`);
+
+
+// const num1 = +prompt("Birinchi sonni kiriting");
+// const num2 = +prompt("Ikkinchi sonni kiriting");
+// const num3 = +prompt("Uchinchi sonni kiriting");
+// let counter = 0;
+// let result;
+// if (num1 > 0) {
+//   counter++;
+// }
+// if (num2 > 0) {
+//   counter++;
+// }
+// if (num3 > 0) {
+//   counter++;
+// }
+// result = counter;
+// alert(result + ` ta musbat son bor`);
+
+
+// const num1 = +prompt("Birinchi sonni kiriting");
+// const num2 = +prompt("Ikkinchi sonni kiriting");
+// const num3 = +prompt("Uchinchi sonni kiriting");
+// let counter = 0;
+// let manfiy = 0
+// let result;
+// if (num1 > 0) {
+//   counter++;
+// }
+// if(num1 < 0) {
+//     manfiy++
+// }
+// if (num2 > 0) {
+//   counter++;
+// }
+// if(num2 < 0) {
+//     manfiy++
+// }
+// if (num3 > 0) {
+//   counter++;
+// }
+// if(num3 < 0) {
+//     manfiy++
+// }
+// result = counter;
+// alert(result + ` ta musbat son bor va  ` +  manfiy + `  ta manfiy son bor` );
+// result = manfiy;
+
+
+// let son1 = +prompt('birinchi sonni kiriting')
+// let son2 = +prompt('ikkinchi sonni kirirting')
+// if (son1 > son2) {
+//     alert(son1 + ` katta`)
+// }else {
+//     alert(son2 + ` katta`)
+// }
+
+
+// let son1 = +prompt('birinchi sonni kiriting')
+// let son2 = +prompt('ikkinchi sonni kirirting')
+// if (son1 < son2) {
+//     alert(son1 + ` kichkina`)
+// }else {
+//     alert(son2 + ` kichkina`)
+// }
+
